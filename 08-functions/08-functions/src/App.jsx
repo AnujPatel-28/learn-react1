fa7ae8056b6941c4f2a3382dc3fb24bec01c2618
Anuj <anujpatel28104@gmail.com>
2026-01-18 15:00:42 +0530

    08-functions

---
+++ 08-functions/08-functions/src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const App = () => {
+  function btnClicked() {
+    console.log('Button clicked!')
+  }
+function inputChanging(val) {
+    console.log(val);
+  }
+ const pageScroll = (elem) => { 
+  if(elem>0){
+    console.log('scrolling down');
+  } else {
+    console.log('scrolling up');
+  }
+  }
+
+
+  return (
+    <div>
+      <button onClick={btnClicked}>Change user</button>
+      <button onClick={() => console.log('Explore button clicked!')}>explore this</button>
+      <input onChange={function(elem){
+         inputChanging(elem.target.value)
+      }} type='text'placeholder='enter name'/>
+
+      <div onMouseMove={(elem) => {
+          console.log('elem.clientX:', elem.clientX, 'elem.clientY:', elem.clientY);
+        }
+      } className='box'>
+      </div>
+      <div onWheel={(elem) => {
+         
+          pageScroll(elem.deltaY);
+
+       } } className='page'>
+      <div className='Page1'></div>
+      <div className='Page2'></div>
+      <div className='Page3'></div>
+      </div> 
+    
+    </div>
+  )
+}
+
+export default App
