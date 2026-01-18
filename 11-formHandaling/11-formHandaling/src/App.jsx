6eb70de591a4132039a1ece239e4289c52a780a2
Anuj <anujpatel28104@gmail.com>
2026-01-18 22:20:31 +0530

    11-formHandaling

---
+++ 11-formHandaling/11-formHandaling/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+ /*e.preventDEfault here prevents its default behaviour of form to be page reload after the submit
+  const submitHandeler = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  }
+
+  <form onSubmit= {(e)=>{submitHandeler}}>
+        <input type="text" placeholder="Enter your name"/>
+        <button>Submit</button>
+  </form>
+
+  now we will for two way binding for the inputs 
+    */
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
